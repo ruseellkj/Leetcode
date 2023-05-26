b1dca0eda4191849fa6d5aad69daa19945fdf2dc
Rushil Jariwala <rkjjariwala@gmail.com>
2023-05-26 16:06:46 +0400

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -7,77 +7,55 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 class Solution {
-  private:
-    void toposort(int node, vector<pair<int,int>> adj[], vector<int>& vis, stack<int>& st){
-        vis[node] = 1;
-        
-        // traversing the neighbors of node
-        for(auto it : adj[node]){
-            int v = it.first;
-            if(!vis[v]){
-                toposort(v,adj,vis,st);
-            }
-        }
-        st.push(node);
+private:
+  void toposort(int node, vector<pair<int, int>> adj[],
+  stack<int>& st, vector<int>& vis) {
+      
+    vis[node] = 1;
+    for (auto it : adj[node]) {
+      int v = it.first;
+      if (!vis[v])
+        toposort(v, adj, st, vis);
     }
-    
-  public:
-    vector<int> shortestPath(int N, int M, vector<vector<int>>& edges)
-    {
-    // Make the graph
-        vector<pair<int,int>> adj[N];
-        for(int i = 0; i < M; i++)
-        {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int wt = edges[i][2];
-            adj[u].push_back({v,wt});
-        }
-
-        // Step 1: Topological Sort
-
-        // Make the vis array
-        vector<int> vis(N, 0);
-        stack<int> st;
-        for(int i = 0; i < N; i++)
-        {
-            if(!vis[i]){
-                toposort(i, adj, vis, st);
-            }
-        }
-
-        // Step 2: The dist array
-        // Make the dist array
-        vector<int> dist(N, INT_MAX);
-        // As 0 is the src
-        dist[0] = 0;
-
-        while(!st.empty()){
-            int node = st.top();
-            st.pop();
-
-            // Update the distances
-            if(dist[node] != INT_MAX){
-                for(auto it : adj[node]){
-                    int v = it.first;
-                    int wt = it.second;
-
-                    // Relaxation function
-                    if(dist[node] + wt < dist[v]){
-                        dist[v] = dist[node] + wt;
-                    }
-                }
-            }
-        }
-
-        for(auto &d : dist){
-            if(d == INT_MAX){
-                d = -1;
-            }
-        }
-        return dist;
+    st.push(node);
+  }
+public:
+  vector<int> shortestPath(int N, int M, vector<vector<int>>& edges) {
+    // make the graph
+    vector<pair<int,int>>adj[N];
+    for (int i = 0; i < M; i++) {
+      int u = edges[i][0];
+      int v = edges[i][1];
+      int wt = edges[i][2];
+      adj[u].push_back({ v,wt });
     }
 
+    // step1: Perform the topological Sort
+    vector < int > vis(N, 0);
+    stack < int > st;
+    for (int i = 0; i < N; i++)
+      if (!vis[i])
+        toposort(i, adj, st, vis);
+
+    // Step 2:- Relax all the edges 
+    vector < int > dist(N, 1e9);
+    dist[0] = 0;
+    while (!st.empty()) {
+      int node = st.top();
+      st.pop();
+
+      for (auto it : adj[node]) {
+        int v = it.first;
+        int wt = it.second;
+        if (dist[node] + wt < dist[v])
+          dist[v] = dist[node] + wt;
+      }
+    }
+
+    for (int i = 0; i< N;i++)
+      if (dist[i] == 1e9) dist[i] = -1;
+    return dist;
+  }
 };
 
 
