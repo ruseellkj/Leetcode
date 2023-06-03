7ee7f31baac00c014eb9a0b488856bc26e4f2926
Rushil Jariwala <rkjjariwala@gmail.com>
2023-06-03 23:02:36 +0400

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,63 +3,104 @@
 using namespace std;
 
 // } Driver Code Ends
+
+// mk the class disjointset
+class DisjointSet {
+	vector<int> rank, parent, size;
+public:
+	// mk the parameterised constructor to put in the data
+	DisjointSet(int n) {
+		rank.resize(n + 1, 0);
+		parent.resize(n + 1);
+		size.resize(n + 1);
+		for (int i = 0; i <= n; i++) {
+			parent[i] = i;
+			size[i] = 1;
+		}
+	}
+	// mk the functions
+	int findUpar(int node) {
+		if (node == parent[node])
+			return node;
+		return parent[node] = findUpar(parent[node]);
+	}
+
+	void unionbyRank(int u, int v) {
+		// finding the ultimate parent of both u and v
+		int ulp_u = findUpar(u);
+		int ulp_v = findUpar(v);
+		if (ulp_u == ulp_v) return;
+		if (rank[ulp_u] < rank[ulp_v]) {
+			parent[ulp_u] = ulp_v;
+		}
+		else if (rank[ulp_v] < rank[ulp_u]) {
+			parent[ulp_v] = ulp_u;
+		}
+		else {
+			// if both the ranks are equal
+			// then add either one
+			parent[ulp_v] = ulp_u;
+			rank[ulp_u]++;
+		}
+	}
+
+	void unionbySize(int u, int v) {
+		// finding the ultimate parent of both u and v
+		int ulp_u = findUpar(u);
+		int ulp_v = findUpar(v);
+		if (ulp_u == ulp_v) return;
+
+		if (size[ulp_u] < size[ulp_v]) {
+			parent[ulp_u] = ulp_v;
+			size[ulp_v] += size[ulp_u];
+		}
+		else if (size[ulp_v] < size[ulp_u]) {
+			parent[ulp_v] = ulp_u;
+			size[ulp_u] += size[ulp_v];
+		}
+		else {
+			// if both the sizes are same
+			parent[ulp_v] = ulp_u;
+			size[ulp_u] += size[ulp_v];
+		}
+	}
+};
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        
-        // converting adj matrix to adj list
-        vector<vector<pair<int, int>>> adjLs(V);
-        for (int i = 0; i < V; i++)
-        {
-            for (auto edge : adj[i])
-            {
-                int u = edge[0];
-                int wt = edge[1];
-                adjLs[i].push_back({u, wt});
-                adjLs[u].push_back({i, wt});
+        // code here
+        // by kruskals algo using disjoint set 
+        // mk the adj list
+        vector<pair<int, pair<int,int>>> edges;
+        for(int i =0; i<V; i++){
+            for(auto it : adj[i]){
+                int wt= it[1];
+                int adjnode = it[0];
+                int node = i;
+                edges.push_back({wt,{node, adjnode}});
             }
         }
-        // mk the pq
-        // {wt,node}
-        priority_queue<pair<int,int>,vector<pair<int,int>>,
-        greater<pair<int,int>>> pq;
-        pq.push({0,0});
+        DisjointSet ds(V);
         
-        // mk the vis arr
-        vector<int> vis(V,0);
-        
-        // to store the sum of the MST
-        int sum = 0; 
-        
-        while(!pq.empty()){
-            auto it = pq.top();
+        sort(edges.begin(), edges.end());
+        int mstwt = 0;
+        for(auto it : edges){
             int wt = it.first;
-            int node = it.second;
-            pq.pop();
+            int u = it.second.first;
+            int v = it.second.second;
             
-            // if the node is visited already
-            if(vis[node] == 1) continue;
-            // when we add it to the mst
-            vis[node] = 1;
-            sum += wt;
-            
-            // traversing the neighbors of that node
-            
-            for(auto it : adjLs[node]){
-                int adjnode = it.first;
-                int eW = it.second;
+            if(ds.findUpar(u) != ds.findUpar(v)){
+                mstwt += wt;
+                ds.unionbySize(u,v);
                 
-                if(!vis[adjnode]){
-                    pq.push({eW,adjnode});
-                }
             }
+            
         }
-        
-        return sum;
-        
+        return mstwt;
         
     }
 };
